d26af7b696530e329475ad4815e5e2199b7c3312
gmaaddi <grm76@drexel.edu>
2024-02-28 17:45:18 -0500

    Create Typography.js
    All font styles are now contained in an individual file

---
+++ LowFi_Prototype/safeplate_v2/components/settings/Typography.js
@@ -0,0 +1,63 @@
+  // text styling
+  //naming convention: type(heading,body,or caption); hierarchy(1-5); size(XS,S,M,L,orXL)
+  export const type = {
+  heading1XL: {
+    fontFamily: 'SF-Pro-Text-Bold',
+    fontSize: 32,
+    lineHeight: 48,
+  },
+  heading2L: {
+    fontFamily: 'SF-Pro-Text-Bold',
+    fontSize: 20,
+    lineHeight: 32,
+  },
+  heading3M: {
+    fontFamily: 'SF-Pro-Text-Bold',
+    fontSize: 16,
+    lineHeight: 28,
+  },
+  heading4M: {
+    fontFamily: 'SF-Pro-Text-Medium',
+    fontSize: 14,
+    lineHeight: 24,
+  },
+  heading5M: {
+    fontFamily: 'SF-Pro-Text-Bold',
+    fontSize: 14,
+    lineHeight: 24,
+  },
+
+  // Body text (3 total)
+  body1M: {
+    fontFamily: 'SF-Pro-Text-Regular',
+    fontSize: 16,
+    lineHeight: 28,
+  },
+  body2S: {
+    fontFamily: 'SF-Pro-Text-Regular',
+    fontSize: 14,
+    lineHeight: 24,
+  },
+  body3S: {
+    fontFamily: 'SF-Pro-Text-LightItalic',
+    fontSize: 14,
+    lineHeight: 24,
+  },
+
+  // Captions (3 total)
+  cap1XS: {
+    fontFamily: 'SF-Pro-Text-Bold',
+    fontSize: 12,
+    lineHeight: 22,
+  },
+  cap2XS: {
+    fontFamily: 'SF-Pro-Text-Medium',
+    fontSize: 12,
+    lineHeight: 22,
+  },
+  cap3XS: {
+    fontFamily: 'SF-Pro-Text-LightItalic',
+    fontSize: 12,
+    lineHeight: 22,
+  },
+}
