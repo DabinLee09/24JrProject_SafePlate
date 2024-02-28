62ad9d4abbd1695e7df61fd8e8a2cdb26594b6fc
gmaaddi <grm76@drexel.edu>
2024-02-28 17:46:03 -0500

    Create Colors.js
    All colors now contained in an individual file

---
+++ LowFi_Prototype/safeplate_v2/components/settings/Colors.js
@@ -0,0 +1,20 @@
+export const PrimaryColors = {
+    Green: '#39735D',
+    LightBlue: '#479BEC',
+}
+  
+export const SecondaryColors = {
+    LightGreen: '#53A385',
+    MutedBlue: '#B2C3E9',
+    BrightYellow: '#FECA72',
+    Pink: '#BFFDEB',
+    Brown: '#5F4125',
+    Red: '#EE453E',
+}
+  
+export const TintsColors = {
+    White: '#FFFFFF',
+    CoolGray: '#F3F3F8',
+    WarmGray: '#FAF6F3',
+    Platinum: '#E3E3E3',
+}
