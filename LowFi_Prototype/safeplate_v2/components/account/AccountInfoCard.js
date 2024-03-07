8769dc2322f4b5250e55036a9162564331336303
gmaaddi <grm76@drexel.edu>
2024-03-07 17:22:23 -0500

    Create AccountInfoCard.js
    Created component for the account page with the profile image, name, and username

---
+++ LowFi_Prototype/safeplate_v2/components/account/AccountInfoCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { TintsColors } from '../settings/Colors'; // Import TintsColors from your Colors file
+import Typography, { type } from '../settings/Typography'
+import GlobalStyles from '../settings/GlobalStyle';
+import { spacing } from '../settings/Spacing';
+import AccountPreferences from './AccountPreferences';
+
+const AccountInfoCard = ({ imageSource, name, username }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={imageSource} // Use the imageSource prop passed from the parent component
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View style={styles.rectangle}>
+        <Text style={[type.heading2L, { color: TintsColors.DarkGray, paddingTop: 16}]}>{name}</Text>
+        <Text style={[type.body1M, { color: TintsColors.MediumGray }]}>{username}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  image: {
+    height: 100,
+    width: 105.5,
+    position: 'absolute',
+    top: 0,
+    zIndex: 20,
+  },
+  rectangle: {
+    width: '100%',
+    backgroundColor: TintsColors.White, // Use TintsColors.White for the rectangle background
+    marginTop: 74, // Adjust as needed
+    paddingVertical: 16,
+    alignItems: 'center',
+    shadowColor: 'rgba(0,0,0,0.1)', // Shadow color
+    shadowOffset: { width: 0, height: 4 }, // Shadow offset
+    shadowRadius: 15, // Shadow radius
+    shadowOpacity: 1, // Shadow opacity
+    elevation: 5, // Android shadow elevation
+    ...GlobalStyles.radius1x,
+    zIndex: 10,
+    ...spacing.vert3x
+  }
+});
+
+export default AccountInfoCard;
