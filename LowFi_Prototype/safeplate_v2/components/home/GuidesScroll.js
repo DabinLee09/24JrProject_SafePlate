3850aef0cdd42e4706a4a1f012a67673ec203e04
gmaaddi <grm76@drexel.edu>
2024-03-08 18:04:18 -0500

    Create horizontal scroll carousel for curated guides
    These are clickable so each dietary image will navigate to the correct guide content when clicked

---
+++ LowFi_Prototype/safeplate_v2/components/home/GuidesScroll.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Image, ScrollView, StyleSheet, Pressable } from 'react-native';
+import { spacing } from '../settings/Spacing';
+
+const GuidesScroll = ({ images, navigation }) => {
+  const handleImagePress = (restriction) => {
+    navigation.navigate('CuratedGuide', { guide: restriction });
+  };
+
+  return (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.container}>
+      {images.map((item, index) => (
+        <Pressable key={index} onPress={() => handleImagePress(item.restriction)}>
+          <Image
+            source={item.source}
+            style={styles.image}
+          />
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    ...spacing.vert3x,
+  },
+  image: {
+    width: 171,
+    height: 256,
+    marginRight: 16,
+  },
+});
+
+export default GuidesScroll;
